a90128e298f0c598ea0e2dc0b43eb9c38dda3e55
Abidin Çelik <abidincelik@msn.com>
2019-12-10 14:57:53 +0300

    Calcite index.js eklemesi

--- src/index.js
+++ src/index.js
@@ -2,7 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import { WebMapView } from './components/Arcgis3D';
+import CalciteThemeProvider from 'calcite-react/CalciteThemeProvider';
 
-ReactDOM.render(<WebMapView />,
+ReactDOM.render(
+    <CalciteThemeProvider>
+        <WebMapView />
+    </CalciteThemeProvider>,
     document.getElementById('root'),
 );
